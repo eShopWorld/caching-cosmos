908a1d024b6280f99adee9ec10534a8c0c1b4124
Ricardo Rodrigues <rrodrigues@eshopworld.com>
2019-07-18 10:27:44 +0100

    updated the desc of the new setting

--- src/Eshopworld.Caching.Cosmos/CosmosCacheFactorySettings.cs
+++ src/Eshopworld.Caching.Cosmos/CosmosCacheFactorySettings.cs
@@ -8,9 +8,13 @@
         public bool UseKeyAsPartitionKey { get; set; }
         public CosmosCacheFactoryIndexingSettings IndexingSettings { get; set; } = new CosmosCacheFactoryIndexingSettings();
         /// <summary>
-        /// If the Database does NOT have a user-defined RU setting and this setting is set to true,
-        /// any on-the-fly created Collection will still default to 400 RUs
+        /// Enables using shared throughput provisioned on a database.
         /// </summary>
+        /// <remarks>
+        /// When disabled, collections will be created with dedicated throughput defined by <see cref="NewCollectionDefaultDTU" />.
+        /// When enabled new collections will share the throughput provisioned on the parent database.
+        /// If the database has no provisioned throughput then new collections will have the dedicated default throughput (400 RUs).
+        /// </remarks>
         public bool UseDatabaseSharedThroughput { get; set; }
 
         public static readonly CosmosCacheFactorySettings Default = new CosmosCacheFactorySettings();
